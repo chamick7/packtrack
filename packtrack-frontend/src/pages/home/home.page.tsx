1b3ac8537de98c4a341b471357a5cd25d418cca8
chamick7 <sw.chal7.dev@gmail.com>
2022-02-28 14:35:29 +0700

    change home directory's name

--- packtrack-frontend/src/pages/home/home.page.tsx
+++ packtrack-frontend/src/pages/home/home.page.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
 
 import Header from "../../components/menu/header.component";
-import FirstHome from "../../components/้home/first-home.component";
-import SecondHome from "../../components/้home/second-home.component";
-import ThirdHome from "../../components/้home/third-home.component";
-import FourHome from "../../components/้home/four-home.component";
-import FiveHome from "../../components/้home/five-home.component";
+import FirstHome from "../../components/home/first-home.component";
+import SecondHome from "../../components/home/second-home.component";
+import ThirdHome from "../../components/home/third-home.component";
+import FourHome from "../../components/home/four-home.component";
+import FiveHome from "../../components/home/five-home.component";
 import Footer from "../../components/menu/footer.component";
 import { useSearchParams, useLocation } from "react-router-dom";
 import * as Scroll from "react-scroll";
